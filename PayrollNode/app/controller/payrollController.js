f27813b5eb9b9b3b84b706594eecddae0f9530d7
James <jwlayug@gmail.com>
2014-02-04 15:21:17 +0800

    initial commit
    initial commit

---
+++ PayrollNode/app/controller/payrollController.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+payrollApp.controller('payrollController', 
+        function payrollController($scope) {
+
+            $scope.account = {
+                user: [
+                    {   user_id: '1',
+                        username: 'jwlayug',
+                        password:'123'},
+                    {   user_id: '2',
+                        username: 'jwlayug2',
+                        password: '1232'}
+
+                ]
+            }
+         
+            
+
+        }
+    
+    );
